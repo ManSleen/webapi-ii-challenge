c68d4e8bd82bd88c7f677b3b0255fc9d546a37f1
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-06 14:03:49 -0600

    Added get post by ID route

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,3 +1,31 @@
 const router = require("express").Router();
 
+const Posts = require("../data/db");
+
+// GET all posts
+router.get("/", (req, res) => {
+  Posts.find()
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "The list of posts could not be retrieved" });
+    });
+});
+
+// GET posts by ID
+router.get("/:id", (req, res) => {
+  console.log(req.params.id);
+  postId = req.params.id;
+  Posts.findById(postId)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Error retrieving post" });
+    });
+});
+
 module.exports = router;
